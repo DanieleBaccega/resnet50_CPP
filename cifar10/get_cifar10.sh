3ef09a3f25c82fed29467c1be6286ee9f12cd1ed
Paolo Viviani <paolo.viviani@unito.it>
2018-11-27 14:39:46 +0000

    cleaned up repo

---
+++ cifar10/get_cifar10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://data.mxnet.io/data/cifar10/cifar10_train.lst
+wget http://data.mxnet.io/data/cifar10/cifar10_train.rec
+wget http://data.mxnet.io/data/cifar10/cifar10_val.lst
+wget http://data.mxnet.io/data/cifar10/cifar10_val.rec
